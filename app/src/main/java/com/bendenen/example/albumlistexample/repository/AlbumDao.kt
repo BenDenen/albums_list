89ab459b604ae49d60643ff8fe90632e2cc1a43c
Boris Denisenko <Shumer512@gmail.com>
2018-12-14 15:28:56 +0300

    Add base database system. Add Album Dao

---
+++ app/src/main/java/com/bendenen/example/albumlistexample/repository/AlbumDao.kt
@@ -0,0 +1,18 @@
+package com.bendenen.example.albumlistexample.repository
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.bendenen.example.albumlistexample.models.Album
+
+@Dao
+interface AlbumDao {
+
+    @Query("SELECT * FROM album")
+    fun queryAlbumList(): LiveData<List<Album>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertAlbum(album: Album)
+}
