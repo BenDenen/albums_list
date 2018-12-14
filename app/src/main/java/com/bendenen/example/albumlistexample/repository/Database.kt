6c87b17de4b0c808ac7e9ec6edb82c9f889718b7
Boris Denisenko <Shumer512@gmail.com>
2018-12-14 15:28:56 +0300

    Add base database system. Add Album Dao

---
+++ app/src/main/java/com/bendenen/example/albumlistexample/repository/Database.kt
@@ -0,0 +1,10 @@
+package com.bendenen.example.albumlistexample.repository
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.bendenen.example.albumlistexample.models.Album
+
+@Database(entities = [Album::class], version = 1)
+abstract class Database : RoomDatabase() {
+    abstract fun albumDao(): AlbumDao
+}
